7034a7def2944a65c4b49369958784c2a98e544f
FunkyFlier <mike@kaslingaircraft.com>
2015-03-20 12:09:30 -0500

    Change ublox parser to handle only PVT message

--- UBLOXL.h
+++ UBLOXL.h
@@ -6,7 +6,7 @@
 
 //#define PORTNUMBER 3
 //#define gpsPort Serial3
-
+#define MSG_LEN 92//only using UBX-NAV-PVT 0x01 0x07 class and id
 
 #define RADIUS_EARTH 6372795
 #define gpsPort Serial3
@@ -15,25 +15,48 @@
 
 typedef union{
   struct{
-    uint32_t iTWO;//0
-    int32_t lon;//4
-    int32_t lat;//8
-    int32_t height;//12
-    int32_t hMSL;//16
-    uint32_t hAcc;//20
-    uint32_t vAcc;//24
-    int32_t velN;//28
-    int32_t velE;//32
-    int32_t velD;//36
-    uint32_t speed3D;//40
-    uint32_t speed2D;//44
-    int32_t heading;//48
-    uint32_t sAcc;//52
-    uint32_t cAcc;//56
-    volatile uint8_t gpsFix;
+    uint32_t iTWO;
+    uint16_t year;
+    uint8_t month;
+    uint8_t day;
+    uint8_t hour;
+    uint8_t min;
+    uint8_t sec;
+    uint8_t valid;
+    uint32_t tAcc;
+    int32_t nano;
+    uint8_t gpsFix;
+    uint8_t flags;
+    uint8_t rsvd1;
+    uint8_t numSV;
+    int32_t lon;
+    int32_t lat;
+    int32_t height;
+    int32_t hMSL;
+    uint32_t hAcc;
+    uint32_t vAcc;
+    int32_t velN;
+    int32_t velE;
+    int32_t velD;
+    int32_t gSpeed;
+    int32_t headMot;
+    uint32_t sAcc;
+    uint32_t headAcc;
+    uint16_t pDop;
+    uint8_t rsvd2;
+    uint8_t rsvd3;
+    uint8_t rsvd4;
+    uint8_t rsvd5;
+    uint8_t rsvd6;
+    uint8_t rsvd7;
+    int32_t headVeh;
+    uint8_t rsvd8;
+    uint8_t rsvd9;
+    uint8_t rsvd10;
+    uint8_t rsvd11;
   }
   vars;
-  byte buffer[61];
+  byte buffer[92];
 }
 GPS_Union_t;
 
@@ -57,7 +80,7 @@ private:
   float lat1_f;
   float lat2_f;
   uint16_t msgLength;
-  uint8_t index,msgLengthLSB,msgLengthMSB,msgType,inBuffer[50],localSumA,localSumB;
+  uint8_t index,msgLengthLSB,msgLengthMSB,msgType,inBuffer[92],localSumA,localSumB;
   
   
 };
